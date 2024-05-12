b3fad0ce443eaf3a716c2303312aec6641ddf5c4
Nima-sltn <nimaso8228@gmail.com>
2024-05-12 21:02:37 +0330

    feat(CompleteProfile): add component logic

--- src/features/authentication/CompleteProfileform.jsx
+++ src/features/authentication/CompleteProfileform.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 import TextField from "./../../ui/TextField";
+import RadioInput from "../../ui/RadioInput";
+import { useMutation } from "@tanstack/react-query";
+import { completeProfile } from "./../../services/authService";
+import toast from "react-hot-toast";
 
 function CompleteProfileform() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: completeProfile,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { message, user } = await mutateAsync({ name, email, role });
+      console.log(user, message);
+      toast.success(message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   return (
     <div
       className="flex justify-center pt-20
     ">
       <div className="w-full sm:max-w-sm">
-        <form className="space-y-8">
+        <form className="space-y-8" onSubmit={handleSubmit}>
           <TextField
             label={"نام و نام خانوادگی"}
             name={"name"}
@@ -24,26 +43,22 @@ function CompleteProfileform() {
             value={email}
           />
           <div className="flex items-center justify-center gap-x-8">
-            <div className="flex items-center gap-x-2 text-secondary-600">
-              <input
-                className="cursor-pointer w-4 h-4 form-radio"
-                type="radio"
-                name="role"
-                id="OWNER"
-                value="OWNER"
-              />
-              <label htmlFor="OWNER">کارفرما</label>
-            </div>
-            <div className="flex items-center gap-x-2 text-secondary-600">
-              <input
-                className="cursor-pointer w-4 h-4 form-radio"
-                type="radio"
-                name="role"
-                id="FREELANCER"
-                value="FREELANCER"
-              />
-              <label htmlFor="FREELANCER">فریلنسر</label>
-            </div>
+            <RadioInput
+              label="کارفرما"
+              name="role"
+              id="OWNER"
+              value="OWNER"
+              onChange={(e) => setRole(e.target.value)}
+              checked={role === "OWNER"}
+            />
+            <RadioInput
+              label="فریلنسر"
+              name="role"
+              id="FREELANCER"
+              value="FREELANCER"
+              onChange={(e) => setRole(e.target.value)}
+              checked={role === "FREELANCER"}
+            />
           </div>
           <button className="btn btn--primary w-full">تایید</button>
         </form>
--- src/services/authService.js
+++ src/services/authService.js
@@ -7,3 +7,9 @@ export function getOtp(data) {
 export function checkOtp(data) {
   return http.post("/user/check-otp", data).then(({ data }) => data.data);
 }
+
+export function completeProfile(data) {
+  return http
+    .post("/user/complete-profile", data)
+    .then(({ data }) => data.data);
+}
