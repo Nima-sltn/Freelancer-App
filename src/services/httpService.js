b9d72d620a1a1016d03e6e19bea6f87c9e7767ca
Nima-sltn <nimaso8228@gmail.com>
2024-05-15 19:09:21 +0330

    feature(httpService): implement access token renewal with refresh token

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:5000/api";
+
 const app = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
@@ -14,3 +16,28 @@ const http = {
 };
 
 export default http;
+
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(`${BASE_URL}/use/refresh-token`, {
+          withCredentials: true,
+        });
+        if (data) return app(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
