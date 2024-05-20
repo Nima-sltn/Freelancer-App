d0723f05146fda54cc76d50c9f6bb64c05946c84
Nima-sltn <nimaso8228@gmail.com>
2024-05-20 20:17:22 +0330

    feat(projectsTable): implement utils function for projectsTable

--- src/features/projects/ProjectsTable.jsx
+++ src/features/projects/ProjectsTable.jsx
@@ -1,6 +1,9 @@
 import useOwnerProjects from "./useOwnerProjects";
 import Loading from "./../../ui/Loading";
 import Empty from "./../../ui/Empty";
+import truncateText from "./../../utils/truncateText";
+import toLocalDateShort from "../../utils/toLocalDateShort";
+import { toPersianNumbersWithComma } from "./../../utils/toPersianNumbers";
 
 function ProjectsTable() {
   const { projects, isLoading } = useOwnerProjects();
@@ -29,10 +32,10 @@ function ProjectsTable() {
           {projects.map((project, index) => (
             <tr key={project._id}>
               <td>{index + 1}</td>
-              <td>{project.title}</td>
+              <td>{truncateText(project.title, 30)}</td>
               <td>{project.category.title}</td>
-              <td>{project.budget}</td>
-              <td>{project.deadline}</td>
+              <td>{toPersianNumbersWithComma(project.budget)}</td>
+              <td>{toLocalDateShort(project.deadline)}</td>
               <td>
                 <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
                   {project.tags.map((tag) => (
