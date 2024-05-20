f56875bf02cf5829f7af8d7349911239425e29d5
Nima-sltn <nimaso8228@gmail.com>
2024-05-20 21:51:08 +0330

    feat(Modal): create modal

--- src/features/projects/ProjectRow.jsx
+++ src/features/projects/ProjectRow.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+import Modal from "../../ui/Modal";
 import Table from "../../ui/Table";
 import toLocalDateShort from "../../utils/toLocalDateShort";
 import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
 import truncateText from "../../utils/truncateText";
+import { HiOutlineTrash } from "react-icons/hi";
+import { TbPencilMinus } from "react-icons/tb";
 
 function ProjectRow({ project, index }) {
+  const [isEditOpen, setIsEditOpen] = useState(false);
+
   return (
     <Table.Row key={project._id}>
       <td>{index + 1}</td>
@@ -28,7 +34,22 @@ function ProjectRow({ project, index }) {
           <span className="badge badge--danger">بسته</span>
         )}
       </td>
-      <td>...</td>
+      <td>
+        <div className="flex items-center gap-x-4">
+          <button onClick={() => setIsEditOpen(true)}>
+            <TbPencilMinus className="h-5 w-5 text-primary-900" />
+          </button>
+          <Modal
+            title="modal title"
+            isOpen={isEditOpen}
+            onClose={() => setIsEditOpen(false)}>
+            This is Modal...
+          </Modal>
+          <button>
+            <HiOutlineTrash className="h-5 w-5 text-error" />
+          </button>
+        </div>
+      </td>
     </Table.Row>
   );
 }
