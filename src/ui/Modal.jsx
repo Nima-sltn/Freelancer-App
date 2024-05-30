a0ed3bbcab1104f1248eaca2f36c6cc3d9816ac5
Nima-sltn <nimaso8228@gmail.com>
2024-05-30 14:07:30 +0330

    feat(hooks): create useOutsideClick custom hook to close modals

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,12 +1,16 @@
 import { HiOutlineX } from "react-icons/hi";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 function Modal({ isOpen, onClose, title, children }) {
+  const ref = useOutsideClick(onClose);
+
   return (
     isOpen && (
       <div
         className="backdrop-blur-sm fixed top-0 left-0 w-full
        h-screen bg-secondary-800 bg-opacity-30 z-50">
         <div
+          ref={ref}
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
         rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
          w-[calc(100vw-2rem)] md:max-w-sm max-h-[calc(100vh-2rem)] overflow-y-auto
