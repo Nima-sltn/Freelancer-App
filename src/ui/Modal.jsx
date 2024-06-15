81e20190ad2f65946e92536f023819f47e50e36c
Nima-sltn <nimaso8228@gmail.com>
2024-06-15 13:05:38 +0330

    fix(modal): fix scrollbar position to rightside of the page

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -12,18 +12,20 @@ function Modal({ isOpen, onClose, title, children }) {
         <div
           ref={ref}
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-        rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
-         w-[calc(100vw-2rem)] md:max-w-sm max-h-[calc(100vh-2rem)] overflow-y-auto
-        ">
-          <div
-            className="flex items-center justify-between 
+            rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
+            w-[calc(100vw-2rem)] md:max-w-lg max-h-[calc(100vh-2rem)] overflow-y-auto
+            scrollbar">
+          <span className="flipped">
+            <div
+              className="flex items-center justify-between 
           border-b border-b-secondary-300 pb-2 mb-6">
-            <p className="text-secondary-700 font-bold text-base">{title}</p>
-            <button onClick={onClose}>
-              <HiOutlineX className="w-5 h-5 text-secondary-500" />
-            </button>
-          </div>
-          {children}
+              <p className="text-secondary-700 font-bold text-base">{title}</p>
+              <button onClick={onClose}>
+                <HiOutlineX className="w-5 h-5 text-secondary-500" />
+              </button>
+            </div>
+            {children}
+          </span>
         </div>
       </div>
     )
