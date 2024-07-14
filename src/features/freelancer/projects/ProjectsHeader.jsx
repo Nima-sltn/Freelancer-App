ae58f515fba5b1ffa04a2edd0596c7cea3595e26
Nima-sltn <nimaso8228@gmail.com>
2024-07-14 21:07:44 +0330

    feat(ProjectHeader): add filter-drop-down to filter projects

--- src/features/freelancer/projects/ProjectsHeader.jsx
+++ src/features/freelancer/projects/ProjectsHeader.jsx
@@ -1,8 +1,20 @@
+import FilterDropDown from "../../../ui/FilterDropDown";
+import useCategories from "./../../../hooks/useCategories";
+
 function ProjectsHeader() {
+  const { transformedCategories } = useCategories();
   return (
     <div className="flex items-center justify-between text-secondary-700 mb-8">
       <h1 className="text-lg font-bold">لیست پروژه ها</h1>
-      <div>فیلتر ها</div>
+      <div>
+        <FilterDropDown
+          filterField={"category"}
+          options={[
+            { value: "ALL", label: "دسته بندی (همه)" },
+            ...transformedCategories,
+          ]}
+        />
+      </div>
     </div>
   );
 }
